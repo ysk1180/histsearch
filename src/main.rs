fd06d662a39aa2ffcde224d2cbd6932582d3bcab
Yusuke Miyazaki <ysk1180@gmail.com>
2021-07-25 09:16:09 +0900

    エラーしている行を非表示にした

--- src/main.rs
+++ src/main.rs
@@ -24,17 +24,43 @@ fn main() {
 
     let reader = BufReader::new(file);
 
+    // readerでエラーしているもの（おそらく日本語などのASCIIコード以外が入っていたもの）を排除する
+    let mut commands: Vec<String> = reader.lines().filter_map(|line| line.ok()).collect();
+    commands.reverse(); // 最近打ったコマンドを上位に表示させたいので新しい順にしている
+
+    // TODO: 内容がない行の削除
+    // TODO: 先頭の不要な文字の削除
+
     let mut i = 0;
-
-    for line in reader.lines() {
-        let line = line.unwrap();
+    for line in commands.iter() {
         println!("{}", line);
-
         i += 1;
-        if i == 10 {
+        if i == 100 {
             break;
         }
     }
+
+    // let mut commands: Vec<_> = reader
+    //     .lines()
+    //     .map(|line| {
+    //         let line = line.unwrap();
+    //         let splited: Vec<&str> = line.split(":0;").collect();
+    //         splited.get(1).unwrap();
+    //     })
+    //     .collect();
+    // commands.reverse();
+
+    // for line in reader.lines() {
+    //     let line = line.unwrap();
+    //     let vec: Vec<&str> = line.split(":0;").collect();
+    //     let command = vec.get(1).unwrap();
+    //     println!("{}", command);
+
+    //     i += 1;
+    //     if i == 10 {
+    //         break;
+    //     }
+    // }
 }
 
 fn find_history_file() -> Option<PathBuf> {
