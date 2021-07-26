d7d04ace192cd8393c94d1be79e2d63041d908c7
Yusuke Miyazaki <ysk1180@gmail.com>
2021-07-27 08:38:46 +0900

    コマンドだけを抽出できるようにした

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::{
     path::PathBuf,
 };
 
+use regex::Regex;
+
 fn main() {
     println!("Input word for search!");
 
@@ -25,11 +27,25 @@ fn main() {
     let reader = BufReader::new(file);
 
     // readerでエラーしているもの（おそらく日本語などのASCIIコード以外が入っていたもの）を排除する
-    let mut commands: Vec<String> = reader.lines().filter_map(|line| line.ok()).collect();
+    let commands: Vec<String> = reader.lines().filter_map(|line| line.ok()).collect();
+
+    // 空行や複数行コマンドの2行目以降などの不要なデータを排除する
+    let re = Regex::new(r":0;").unwrap();
+    let mut commands: Vec<String> = commands
+        .iter()
+        .filter(|command| re.is_match(command))
+        .map(|c| c.to_string())
+        .collect();
+
     commands.reverse(); // 最近打ったコマンドを上位に表示させたいので新しい順にしている
 
-    // TODO: 内容がない行の削除
-    // TODO: 先頭の不要な文字の削除
+    // 「: 1627024977:0;ts」みたいな形式で入っているのでコマンドだけを抽出する
+    let re = Regex::new(r"^.+:0;").unwrap();
+    let commands: Vec<String> = commands
+        .iter()
+        .map(|c| re.replace(c, ""))
+        .map(|c| c.to_string())
+        .collect();
 
     let mut i = 0;
     for line in commands.iter() {
@@ -39,28 +55,6 @@ fn main() {
             break;
         }
     }
-
-    // let mut commands: Vec<_> = reader
-    //     .lines()
-    //     .map(|line| {
-    //         let line = line.unwrap();
-    //         let splited: Vec<&str> = line.split(":0;").collect();
-    //         splited.get(1).unwrap();
-    //     })
-    //     .collect();
-    // commands.reverse();
-
-    // for line in reader.lines() {
-    //     let line = line.unwrap();
-    //     let vec: Vec<&str> = line.split(":0;").collect();
-    //     let command = vec.get(1).unwrap();
-    //     println!("{}", command);
-
-    //     i += 1;
-    //     if i == 10 {
-    //         break;
-    //     }
-    // }
 }
 
 fn find_history_file() -> Option<PathBuf> {
